ffabc216250eb131357fc94d2bf13a4d53c27f14
Shubham Pujara <Shubham6-9>
2026-03-13 00:55:23 +0530

    loops 1

---
+++ loops/ForIn.py
@@ -0,0 +1,12 @@
+# for i in [1, 2, 3]:
+#     print("eww", i)
+
+# for i in range(10):
+#     print(i + 1, end=", ")
+
+while True:
+    number = int(input("Enter number"))
+    if(number>0):
+        for i in range(number):
+            print("yesys")
+        break
